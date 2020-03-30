9c1671b73c8168a950bdd466b8b4671a0bb5c71a
Emmanuel <akhigbemmanuel@gmai.com>
2020-03-30 22:58:18 +0100

    Update profile implemented

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -4,6 +4,8 @@ CREATE TABLE users
 (
     id serial PRIMARY KEY,
     name varchar(100),
+    pet varchar(100),
+    age integer,
     email text UNIQUE NOT NULL,
     entries BIGINT DEFAULT 0,
     joined TIMESTAMP NOT NULL
